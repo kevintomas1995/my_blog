cfd4244980222dc871cbd99971b273255150f02b
kevintomas1995 <71970589+kevintomas1995@users.noreply.github.com>
2021-08-27 10:45:56 +0200

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
 
     try {
       client = await MongoClient.connect(
-        "mongodb+srv://<username>:<pw>@cluster0.pqdli.mongodb.net/myBlog?retryWrites=true&w=majority"
+        "mongodb+srv://kevin:XkWJANgOTkACuIMGTzIIokbszqAo@cluster0.pqdli.mongodb.net/myBlog?retryWrites=true&w=majority"
       );
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database" });
